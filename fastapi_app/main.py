bc9788cf71c292731499e6c5d054d3064167f8cb
El Mouttaqui Yaakoub <mmoutaki05@gmail.com>
2025-06-23 14:36:26 -0400

    update

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -38,7 +38,10 @@ app.mount("/dashboard", WSGIMiddleware(app_dash.server))
 user={"admin":"123"}
 
 
-EXTERNAL_API_URL = "http://127.0.0.1:8000/info"
+#EXTERNAL_API_URL = "http://127.0.0.1:8000/info"
+EXTERNAL_API_URL = "https://westher-d9b8edgdgcdrezd4.canadacentral-01.azurewebsites.net/info"
+
+
 
 def get_external_info():
     try:
